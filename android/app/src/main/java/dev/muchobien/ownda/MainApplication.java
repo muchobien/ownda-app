217c28728ccc913badd80115b1f4e67dcdf4e5be
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2022-02-07 22:40:43 +0100

    fix: android build

--- android/app/src/main/java/dev/muchobien/ownda/MainApplication.java
+++ android/app/src/main/java/dev/muchobien/ownda/MainApplication.java
@@ -3,12 +3,14 @@ package dev.muchobien.ownda;
 import android.app.Application;
 import android.content.Context;
 import android.content.res.Configuration;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
 import java.lang.reflect.InvocationTargetException;
@@ -31,6 +33,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+        
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage();
         }
       });
 
