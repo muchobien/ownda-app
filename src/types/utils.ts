3d197196cdf2482f3dba5deda475892d3d90142a
Rodrigo Sanabria <krosf@muchobien.dev>
2022-04-03 16:03:57 +0200

    fix: add more defensive code format

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -10,7 +10,7 @@ import type {
 } from 'react-native';
 
 export type PFC<P = void> = {
-  (_: P): ReactElement | null;
+  (props: P): ReactElement | null;
 };
 
 export type WithoutTypename<T extends { __typename?: unknown }> = {
@@ -19,7 +19,7 @@ export type WithoutTypename<T extends { __typename?: unknown }> = {
 
 export type ListRenderItem<T> = NonNullable<LRI<T>>;
 export type SectionListRenderItem<T> = NonNullable<SRI<T>>;
-export type KeyExtractor<T> = (_item: T, _index: number) => string;
+export type KeyExtractor<T> = (item: T, index: number) => string;
 
 export type SectionRenderHeader<T, U> = NonNullable<
   SectionListProps<T, U>['renderSectionHeader']
