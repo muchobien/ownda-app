44020f309f714a5b16c5440e0d93e10f749ea6ed
Rodrigo Sanabria <krosf@muchobien.dev>
2022-05-29 20:44:27 +0200

    feat: add graph library

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,49 @@
 import type { Screen } from '@app/types';
 import { Text } from '@app/components';
 import { Container } from './styles';
+import { LineGraph } from 'react-native-graph';
+import type { ViewStyle } from 'react-native';
+
+const style: ViewStyle = {
+  height: 200,
+  width: '100%',
+};
 
 export const Home: Screen<'Home'> = () => (
   <Container>
     <Text>Home</Text>
+    <LineGraph
+      enablePanGesture
+      lineThickness={4}
+      style={style}
+      animated
+      points={[
+        { date: new Date(), value: 15 },
+        { date: new Date(), value: -12 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 12 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 23 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 12 },
+        { date: new Date(), value: 23 },
+        { date: new Date(), value: 23 },
+        { date: new Date(), value: 23 },
+        { date: new Date(), value: 43 },
+        { date: new Date(), value: -12 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 12 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 23 },
+        { date: new Date(), value: 2 },
+        { date: new Date(), value: 12 },
+        { date: new Date(), value: 23 },
+        { date: new Date(), value: 23 },
+        { date: new Date(), value: 3 },
+      ]}
+      color="#825cff"
+    />
   </Container>
 );
